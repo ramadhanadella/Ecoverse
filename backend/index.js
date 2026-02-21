a3259d8b4aa82ea003b3073b670355a22178e7f3
Adella Ramadhan <145951591+daehokkim@users.noreply.github.com>
2026-02-21 10:04:50 +0700

    revisi url

--- backend/index.js
+++ backend/index.js
@@ -44,7 +44,7 @@ app.use(
       "http://localhost:5174",
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://ecoverse-frontend.vercel.app",
+      "https://ecoverse-2026.vercel.app",
     ],
   }),
 );
