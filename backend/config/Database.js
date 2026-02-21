d4b3e4120bf9ccea4e5612ceff06433d6cdb9ac9
Adella Ramadhan <145951591+daehokkim@users.noreply.github.com>
2026-02-21 12:44:43 +0700

    db railway

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -8,6 +8,7 @@ const db = new Sequelize(
   process.env.DB_PASSWORD || "",
   {
     host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 3306,
     dialect: "mysql",
     // Tambahan untuk production biasanya butuh ini:
     dialectOptions:
