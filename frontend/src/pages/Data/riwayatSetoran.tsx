01e195b8e85bf5de1c0be97b0cd3ad3c5d045e2a
Adella Ramadhan <145951591+daehokkim@users.noreply.github.com>
2026-02-21 22:06:18 +0700

    setor

--- frontend/src/pages/Data/riwayatSetoran.tsx
+++ frontend/src/pages/Data/riwayatSetoran.tsx
@@ -44,8 +44,8 @@ export default function RiwayatSetoranPage() {
         <h1 className="text-2xl font-semibold mb-4 text-[#000000] dark:text-white">
           Riwayat Setoran
         </h1>
-        {userRole === "koor" ||
-          (userRole === "pekerja" && (
+        {userRole === "pekerja" ||
+          (userRole === "koor" && (
             <button
               className="flex items-center gap-2 px-4 py-2 text-sm font-medium text-white bg-[#285303] rounded hover:bg-[#80BC4D]"
               onClick={openAddModal}
